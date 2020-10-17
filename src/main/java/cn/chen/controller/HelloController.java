fb12e79b9e5bc4292ada01de0db0c421aaf6b093
chenshh <seucsh@163.com>
2020-10-17 11:44:01 +0800

    dev update HelloController

--- src/main/java/cn/chen/controller/HelloController.java
+++ src/main/java/cn/chen/controller/HelloController.java
@@ -10,25 +10,12 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
         return "hello";
     }
 
-    @RequestMapping("/hello2")
-    public String hello2(){
-        System.out.println("hello2");
-        return "hello2";
-    }
-
-    @RequestMapping("/hello3")
-    public String hello3(){
-        System.out.println("hello3");
-        return "hello3";
-    }
-
-    @RequestMapping("/hello4")
-    public String hello4(){
-        System.out.println("hello4");
-        return "hello4";
-    }
 
 }
