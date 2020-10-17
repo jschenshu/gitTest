77a1fe3e9ffc8c5f332eca50ac36b26143661b44
chenshh <seucsh@163.com>
2020-10-17 11:50:11 +0800

    记录时间

--- src/main/java/cn/chen/controller/HelloController.java
+++ src/main/java/cn/chen/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
         System.out.println("hello");
-        System.out.println("hello");
+        System.currentTimeMillis();
         System.out.println("hello");
         System.out.println("hello");
         return "hello";
